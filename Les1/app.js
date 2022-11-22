9bd95b19bdb04b5157a09f823182a68a0468c49f
Kjell <34952@ma-web.nl>
2022-11-22 10:31:49 +0100

    update

---
+++ Les1/app.js
@@ -0,0 +1,26 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello world!")
+        let appNaam = "kaas"
+        let versienummer = 0.9
+        let versiedatum = 22-11
+        let autheur = "Kjell"
+        let copyright = "Niet copy pasten"
+        let distributeur = "Kaas"
+        let darkmode = true
+    }
+
+}
+console.log (appNaam)
+console.log (versienummer)
+console.log (versiedatum)
+console.log (autheur)
+console.log (copyright)
+console.log (distributeur)
+console.log (darkmode)
+
+let app = new App();
+app.runApplication();
+
